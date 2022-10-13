65804a3ee10147c11da449a9b49e2cd7d80feb3f
Anttonino <victor.antonino12@gmail.com>
2022-10-13 11:32:07 -0300

    feat: iniciate nest place
    Begin with the nest structure and download the project dependencies

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot ({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db_green_heart',
+      entities: [],
+      synchronize: true
+    })  
+   ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
