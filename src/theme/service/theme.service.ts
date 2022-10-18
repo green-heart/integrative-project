e61fff53064509cd06a560f17b36fad5e8290b23
Anttonino <victor.antonino12@gmail.com>
2022-10-18 12:15:23 -0300

    feat: add new methods in theme service
    add create, update, and delete methods in theme service

--- src/theme/service/theme.service.ts
+++ src/theme/service/theme.service.ts
@@ -2,39 +2,31 @@ import { Injectable } from "@nestjs/common";
 import { HttpStatus } from "@nestjs/common/enums";
 import { HttpException } from "@nestjs/common/exceptions";
 import { InjectRepository } from "@nestjs/typeorm";
+import { DeleteResult } from "typeorm";
 import { ILike } from "typeorm/find-options/operator/ILike";
 import { Repository } from "typeorm/repository/Repository";
+
 import { Theme } from "../entities/theme.entity";
 
 @Injectable()
 export class ThemeService {
-    delete(id: number) {
-        throw new Error("Method not implemented.");
-    }
-    update(theme: Theme): Promise<Theme> {
-        throw new Error("Method not implemented.");
-    }
-    create(theme: Theme): Promise<Theme> {
-        throw new Error("Method not implemented.");
-    }
     findByDescription(classfication: string): Promise<Theme[]> {
         throw new Error("Method not implemented.");
     }
-    constructor(
-        @InjectRepository(Theme)
-        private themeRepository: Repository<Theme>
-    ) {}
+
+    constructor(@InjectRepository(Theme) private themeRepository: Repository<Theme>) {}
 
     async findAll (): Promise<Theme[]>{
         return await this.themeRepository.find({
+            relations: {posting: true}
         })
     }
 
     async findById (id: number): Promise<Theme> {
         let theme = await this.themeRepository.findOne({
-            where: {id}
-        })
-
+            where: {id},
+            relations: {posting: true}
+        });
         if (!theme)
             throw new HttpException('Tema nÃ£o encontrado', HttpStatus.NOT_FOUND)
         return theme;
@@ -42,16 +34,35 @@ export class ThemeService {
 
     async findByClassification (classification: string): Promise<Theme[]> {
         return await this.themeRepository.find({
-            where: {
-                classification: ILike(`%${classification}%`)
-            }
+            where: {classification: ILike(`%${classification}%`)},
+            relations: {posting: true}
         })
     }
 
     async findByTypes (types: string): Promise<Theme[]> {
         return await this.themeRepository.find({
-            where: {types: ILike(`%${types}%`)}
+            where: {types: ILike(`%${types}%`)},
+            relations: {posting: true}
         })
     }
 
+    async create (theme: Theme): Promise <Theme> {
+        return await this.themeRepository.save(theme);
+    }
+
+    async update (theme: Theme): Promise <Theme> {
+        let searchTheme: Theme = await this.findById(theme.id);
+
+        if(!searchTheme || !theme.id) 
+            throw new HttpException ('Theme not found!', HttpStatus.NOT_FOUND);
+        return await this.themeRepository.save(theme);   
+    }
+
+    async delete (id: number): Promise <DeleteResult> {
+        let searchTheme = await this.findById (id);
+
+        if (!searchTheme)
+            throw new HttpException ('Theme not found!', HttpStatus.NOT_FOUND);
+        return await this.themeRepository.delete(id);
+    }
 }
