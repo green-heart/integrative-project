fbf76f76ed131aeec8e3b80314a6127bc434fa5d
Anttonino <victor.antonino12@gmail.com>
2022-10-18 12:52:34 -0300

    refactor: code restructure (posting controller)

--- src/posting/controllers/posting.controller.ts
+++ src/posting/controllers/posting.controller.ts
@@ -1,5 +1,4 @@
 import { Body, Delete, Param, ParseIntPipe,Controller, Get, HttpCode, HttpStatus, Post, Put } from "@nestjs/common"
-import { PostUsers } from "src/users/users.module";
 import { Posting } from "../entities/posting.entity";
 import { PostingService } from "../services/posting.service";
 
@@ -7,43 +6,35 @@ import { PostingService } from "../services/posting.service";
 export class PostingController {
     constructor (private readonly postService: PostingService) { }
 
-@Get()
-@HttpCode (HttpStatus.OK)
-findAll (): Promise<Posting[]> {
-    return this.postService.findAll ();
-}
+    @Get()
+    @HttpCode (HttpStatus.OK)
+    findAll (): Promise<Posting[]> {
+        return this.postService.findAll ();
+    }
 
-@Get ('/:id')
-@HttpCode(HttpStatus.OK)
-findById (@Param('id', ParseIntPipe)id: number): Promise<Posting> {
-    return this.postService.findById(id)
-}
+    @Get ('/:id')
+    @HttpCode(HttpStatus.OK)
+    findById (@Param('id', ParseIntPipe)id: number): Promise <Posting> {
+        return this.postService.findById(id)
+    }
 
-@Post()
-@HttpCode(HttpStatus.CREATED)
-create(
-    @Body()
-    posting: Posting
-): Promise<Posting> {
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+        create(@Body() posting: Posting): Promise <Posting> {
     return this.postService.create(posting)
-}
+    }
 
-@Put()
-@HttpCode(HttpStatus.OK)
-update (
-    @Body()
-    posting: Posting
-): Promise<Posting> {
+    @Put()
+    @HttpCode(HttpStatus.OK)
+    update (@Body() posting: Posting): Promise <Posting> {
     return this.postService.update(posting);
+    }
+
+    @Delete('/:id')
+    @HttpCode(HttpStatus.NO_CONTENT)
+    delete (@Param('id', ParseIntPipe) id: number) {
+        return this.postService.delete(id)
+    }
 }
 
-@Delete('/:id')
-@HttpCode(HttpStatus.NO_CONTENT)
-delete (@Param('id', ParseIntPipe)
-    id: number
-)   {
-    return this.postService.delete(id)
-}
-
-}
 
