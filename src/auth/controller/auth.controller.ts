28032f17390a5a027028a7ab2e0afd9d219937f6
DaniNasciment <dannni01@hotmail.com>
2022-10-27 14:19:53 -0300

    feat: create auth controller
    Co-authored-by: DaniNasciment <dannni01@hotmail.com>
    Co-authored-by: alexsandermog <alexsandermachado15@gmail.com>

---
+++ src/auth/controller/auth.controller.ts
@@ -0,0 +1,18 @@
+import { Body, Controller, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
+import { LocalAuthGuard } from '../guard/local-auth.guard';
+import { AuthService } from '../services/auth.service';
+import { UserLogin } from './../entities/userlogin.entity';
+
+@Controller("/auth")
+export class AuthController {
+    constructor(private authService: AuthService) { }
+
+    @UseGuards(LocalAuthGuard)
+    @HttpCode(HttpStatus.OK)
+    @Post('/login')
+
+    async login(@Body() user: UserLogin): Promise <any> {
+        return this.authService.login(user);
+    }
+
+}
