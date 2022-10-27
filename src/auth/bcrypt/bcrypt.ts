6fc908b1830bf9a6c01e9e87b822446996f760cd
Anttonino <victor.antonino12@gmail.com>
2022-10-27 14:56:12 -0300

    refactor: change code formatting in auth module
    auth module, entity, service, controller, guard and strategy changed

--- src/auth/bcrypt/bcrypt.ts
+++ src/auth/bcrypt/bcrypt.ts
@@ -1,16 +1,15 @@
 import { Injectable } from "@nestjs/common";
 import * as bcrypt from 'bcrypt'
 
-@Injectable()
+@Injectable ()
 export class Bcrypt {
 
-    async encryptPassword (password: string): Promise<string>{
-
+    async encryptPassword (password: string): Promise <string> {
         let salt: number = 10
-        return await bcrypt.hash(password, salt);
+        return await bcrypt.hash (password, salt);
     }
 
-    async comparePasswords(passwordDatabase: string, passwordTyped:string): Promise <boolean> {
-        return bcrypt.compareSync(passwordTyped, passwordDatabase);
+    async comparePasswords (passwordDatabase: string, passwordTyped:string): Promise <boolean> {
+        return bcrypt.compareSync (passwordTyped, passwordDatabase);
     }
 }
--- src/auth/controller/auth.controller.ts
+++ src/auth/controller/auth.controller.ts
@@ -1,18 +1,18 @@
 import { Body, Controller, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
+
 import { LocalAuthGuard } from '../guard/local-auth.guard';
 import { AuthService } from '../services/auth.service';
 import { UserLogin } from './../entities/userlogin.entity';
 
-@Controller("/auth")
+@Controller ("/auth")
 export class AuthController {
-    constructor(private authService: AuthService) { }
+    constructor (private authService: AuthService) { }
 
-    @UseGuards(LocalAuthGuard)
-    @HttpCode(HttpStatus.OK)
-    @Post('/login')
+    @UseGuards (LocalAuthGuard)
+    @HttpCode (HttpStatus.OK)
+    @Post ('/login')
 
-    async login(@Body() user: UserLogin): Promise <any> {
-        return this.authService.login(user);
+    async login (@Body () user: UserLogin): Promise <any> {
+        return this.authService.login (user);
     }
-
 }
