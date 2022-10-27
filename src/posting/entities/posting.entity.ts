e35deb5ba3dd88779dd7ef0c00b64db334686a2a
KaiqueBourguignon <kaiquegeneration@gmail.com>
2022-10-27 11:08:53 -0300

    feat: create the relation n:1 between user and posting
    Co-authored-by: JoaoVictorCSousa <carvalhojoaovi20@gmail.com>
    Co-authored-by: KaiqueBourguignon <kaiquegeneration@gmail.com>
    Co-authored-by: Popdollss <roseanemelo17@outlook.com>

--- src/posting/entities/posting.entity.ts
+++ src/posting/entities/posting.entity.ts
@@ -1,5 +1,6 @@
 import { IsNotEmpty } from "class-validator";
 import { Theme } from "../../theme/entities/theme.entity";
+import { User } from "../../users/entities/users.entitys";
 import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity ({name: "tb_posting"})
@@ -9,7 +10,7 @@ export class Posting {
     id: number
 
     @IsNotEmpty ()
-    @Column ({length: 1000, nullable: false})
+    @Column ({length: 100, nullable: false})
     text: string
 
     @Column ({length: 255})
@@ -25,4 +26,9 @@ export class Posting {
         onDelete: 'CASCADE'
     })
     theme: Theme
+    
+    @ManyToOne (() => User, (user) => user.posting, {
+        onDelete: 'CASCADE'
+    })
+    user: User
 }
