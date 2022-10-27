27f58ac539158f00c83a4c19a23a0deb306e82fa
Anttonino <victor.antonino12@gmail.com>
2022-10-27 16:57:08 -0300

    fix: change variable name user to username

--- src/auth/strategy/local.strategy.ts
+++ src/auth/strategy/local.strategy.ts
@@ -1,19 +1,18 @@
 import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
 import { Strategy } from "passport-local";
-import { AuthService} from "../services/auth.service";
+import { AuthService } from "../services/auth.service";
 
-
-@Injectable()
+@Injectable ()
 export class LocalStrategy extends PassportStrategy (Strategy) {
     constructor (private authService: AuthService) {
         super ({
-            usernameField: 'user',
+            usernameField: 'username',
             passwordField: 'password'
-        });
+        })
     }
 
-    async validate (username:string, password:string): Promise <string>{
+    async validate (username: string, password: string): Promise <any> {
         const user = await this.authService.validateUser (username, password);
         if (!user) {
             throw new UnauthorizedException ();
