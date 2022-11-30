658f2897be3308d7d773e85f3fb69956615b78cb
João Victor Carvalho <carvalhojoaovi20@gmail.com>
2022-11-30 11:37:26 -0300

    feat: create components and pages
    Co-authored-by: JoaoVictorCSousa <carvalhojoaovi20@gmail.com>
    Co-authored-by: KaiqueBourguignon <kaiquegeneration@gmail.com>

---
+++ src/pages/login/Login.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Grid,Typography, TextField, Button } from '@material-ui/core';
+import {Box} from '@mui/material';
+import { Link } from 'react-router-dom';
+import './Login.css';
+
+function Login() {
+
+    return (
+        <Grid container direction='row' justifyContent='center' alignItems='center'>
+            <Grid alignItems='center' xs={6}>
+                <Box paddingX={20}>
+                    <form>
+                        <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='textos1'>Entrar</Typography>
+                        <TextField id='usuario' label='usuário' variant='outlined' name='usuario' margin='normal' fullWidth />
+                        <TextField id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password'fullWidth />
+                        <Box marginTop={2} textAlign='center'>
+                            <Link to='/home' className='text-decorator-none'>
+                                <Button type='submit' variant='contained' color='primary'>
+                                    Logar
+                                </Button>
+                            </Link>
+                        </Box>
+                    </form>
+                    <Box display='flex' justifyContent='center' marginTop={2}>
+                        <Box marginRight={1}>
+                            <Typography variant='subtitle1' gutterBottom align='center'>Não tem uma conta?</Typography>
+                        </Box>
+                            <Typography variant='subtitle1' gutterBottom align='center' className='textos1'>Cadastre-se</Typography>
+                    </Box>
+                </Box>
+            </Grid>
+            <Grid xs={6} className='imagem'>
+
+            </Grid>
+        </Grid>
+    );
+}
+
+export {Login}
