f8b0d9d0cd73063ac653f069b4026e3882d5691f
Arthur <arthuremeunome@gmail.com>
2022-12-01 10:32:37 -0300

    add: crate imagens component
    Co-authored-by: ArthurMFarias0 <arthuremeunome@gmail.com>
    Co-authored-by: JoaoVictorCSousa <carvalhojoaovi20@gmail.com>
    Co-authored-by: DaniNasciment <dannni01@hotmail.com>
    Co-authored-by: KaiqueBourguignon <kaiquegeneration@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import {Footer} from './components/statics/footer/Footer';
 import {Home} from './pages/home/Home';
 import './App.css';
 import { Title } from './components/title/title';
-import { Subtitles } from './components/subtitles/subtitles'
+import { Subtitles } from '@mui/icons-material';
 import {Login} from './pages/login/Login';
 import { Navbar } from './components/statics/navbar/Navbar';
 import { Posting } from './pages/posting/Posting';
--- src/pages/posting/Posting.tsx
+++ src/pages/posting/Posting.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import {Typography, Box, Grid, Button} from '@material-ui/core';
 import './Posting.css';
-import { Title } from '@mui/icons-material';
+import { Text } from '../../components/text/Text';
 import { Subtitles } from '../../components/subtitles/subtitles';
-
+import { Imagens } from '../../components/imagens/Imagens';
 
 function Posting(){
     return(
         <>
         <Subtitles/>
+        <Text>Projeto de conclusão do Bootcamp Full-Stack (Javascript) da Generation Brasil. O projeto tem como tema o objetivo 13 de desenvolvimento sustentável da ONU.
+O projeto Green Heart se trata de uma rede social que tem como objetivo a mobilização de pessoas engajadas com o combate contra as mudanças climáticas no planeta. </Text>
+        <Imagens/>
             <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem dolore eius totam rem saepe quasi animi? Voluptates temporibus non et. Veniam inventore dolor non temporibus maxime reiciendis, dolorum alias. Et.</h1>
         </>
     )
