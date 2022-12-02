b1bc9ffbf6e31bffe8fe84790d9769d165bab493
João Victor Carvalho <carvalhojoaovi20@gmail.com>
2022-12-02 12:09:20 -0300

    feat: bug fix

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,12 +1,12 @@
-import React, { ChangeEvent, useEffect, useState } from 'react';
+import React, {useState, ChangeEvent, useEffect} from 'react';
 import { Grid,Typography, TextField, Button } from '@material-ui/core';
 import {Box} from '@mui/material';
 import { Link, useNavigate } from 'react-router-dom';
-import './Login.css';
+import {login} from '../../services/Service';
 import useLocalStorage from 'react-use-localstorage';
+import './Login.css';
 import UserLogin from '../../models/UserLogin';
-import { login } from '../../services/Service';
-import { DateRangeSharp } from '@mui/icons-material';
+
 
 function Login() {
 
@@ -35,12 +35,6 @@ function Login() {
             }
         }, [token])
 
-
-
-
-
-        
-
         async function onSubmit(e: ChangeEvent<HTMLFormElement>){
             e.preventDefault();
 
@@ -49,7 +43,7 @@ function Login() {
             
  
                 alert('Logado com sucesso!')  //Sucessfully logged
-            }catch{Error}{
+            }catch(Error){
                 alert(' Dados do usuário inconsistentes. Erro de Login')   //Inconsistent userdata. Login error
             }
 
@@ -62,14 +56,14 @@ function Login() {
         <Grid container direction='row' justifyContent='center' alignItems='center'>
             <Grid alignItems='center' xs={6}>
                 <Box paddingX={20}>
-                    <form>
+                    <form onSubmit={onSubmit}>
                         <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='textos1'>Entrar</Typography>
                         <TextField value={userLogin.user} onChange={(e: ChangeEvent<HTMLInputElement>) =>updatedModel(e)} id='user' label='user' variant='outlined' name='user' margin='normal' fullWidth />
                         <TextField value={userLogin.password} onChange={(e: ChangeEvent<HTMLInputElement>) =>updatedModel(e)} id='password' label='password' variant='outlined' name='password' margin='normal' type='password'fullWidth />
                         <Box marginTop={2} textAlign='center'>
                         
                         <Button type='submit' variant='contained' color='primary'>
-                                    Login
+                                    Logar
                                 </Button>
                             
                         </Box>
@@ -78,11 +72,14 @@ function Login() {
                         <Box marginRight={1}>
                             <Typography variant='subtitle1' gutterBottom align='center'>Não tem uma conta?</Typography>
                         </Box>
-                            <Typography variant='subtitle1' gutterBottom align='center' className='textos1'>Cadastre-se</Typography>
+                        <Link to='/registeruser'>
+                            <Typography variant='subtitle1' gutterBottom align='center' className='text1'>Cadastre-se</Typography>
+                            </Link>
                     </Box>
+
                 </Box>
             </Grid>
-            <Grid xs={6} className='imagem'>
+            <Grid xs={6} className='image'>
 
             </Grid>
         </Grid>
