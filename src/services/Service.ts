7d3440b0ec58e4762babed8046f5b61de5009590
João Victor Carvalho <carvalhojoaovi20@gmail.com>
2022-12-08 10:43:07 -0300

    feat: add more components in service

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -13,3 +13,26 @@ export const login = async(url: any, dados: any, setDado: any) => {
     const response = await api.post(url, dados)
     setDado(response.data)
 }
+
+export const busca = async (url: any, setDado: any, header: any) => {
+    const resposta = await api.get(url, header)
+}
+
+export const buscaId = async (url: any, setDado: any, header: any) => {
+    const resposta = await api.get(url,header)
+    setDado(resposta.data)
+}
+
+export const post = async(url: any, dados: any, setDado: any, header: any) => { 
+    const resposta = await api.post(url,dados,header)
+    setDado(resposta.data)
+}
+
+export const put = async(url: any, dados: any, setDado: any, header: any) => { 
+    const resposta = await api.put(url,dados,header)
+    setDado(resposta.data)
+}
+
+export const deleteId = async(url: any,header: any) => { 
+    await api.delete(url,header)
+}
