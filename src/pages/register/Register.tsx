1d9e9f6f06b65e0d76778c431741ac1b658d85d7
Anttonino <victor.antonino12@gmail.com>
2022-12-15 16:40:54 -0300

    rearrange: change the register function name
    Co-authored-by: Anttonino <victor.antonino12@gmail.com>
    Co-authored-by: alexsandermog <alexsandermachado15@gmail.com>

--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -5,7 +5,7 @@ import './Register.css';
 import User from '../../models/User';
 import { register } from '../../services/Service';
 
-function RegisterUser(){
+function Register(){
 
     let navigate = useNavigate();
     const [confirmPassword, setConfirmPassword] = useState<string>("")
@@ -49,50 +49,49 @@ function RegisterUser(){
     async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
         e.preventDefault()
         if(confirmPassword === user.password){
-        register(`/users/sign_up`, user, setUserResult)
+        register(`/users/register`, user, setUserResult)
         alert('Usuario cadastrado com sucesso')
-        back()
         }else{
             alert('Dados inconsistentes. Favor verificar as informações de cadastro.')
         }
-
     }
+      
 
-    function back () {
-        navigate ('/login')
-    }
+    return (
+        <Grid container direction='column' justifyContent='center' alignItems='center'>
+            <Grid item xs={6} className='image2'></Grid>
+            <Grid item xs={6} alignItems='center'>
+                <Box padding={10}>
+                    <form onSubmit={onSubmit}> 
+                        <Typography variant='h3' gutterBottom  component='h3' align='center' className='text2'>𝙲𝚊𝚍𝚊𝚜𝚝𝚛𝚊𝚛</Typography>
+                        <TextField  value={user.name} onChange={(e: ChangeEvent<HTMLInputElement>) =>updatedModel(e)} required id='name' label='Nome' variant='outlined' name='name' margin='normal' />
+                        <TextField value={user.username} onChange={(e: ChangeEvent<HTMLInputElement>) =>updatedModel(e)} required id='username' label='Username' variant='outlined' name='username' margin='normal' />
+                       
+                        <div> <TextField value={user.email} onChange={(e: ChangeEvent<HTMLInputElement>) =>updatedModel(e)} required id='email' label='Email' variant='outlined' name='email' margin='normal' type='email' fullWidth  /></div>
+                        <TextField value={user.password} onChange={(e: ChangeEvent<HTMLInputElement>) =>updatedModel(e)} required id='password' label='Senha' variant='outlined' name='password' margin='normal' type='password' />
+                        <TextField value={confirmPassword} onChange={(e: ChangeEvent<HTMLInputElement>) =>confirmPasswordHandle(e)}required id='confirmPassword' label='Confirme sua senha' variant='outlined' name='confirmPassword' margin='normal' type='password' />
+                        <Box marginTop={2} textAlign='center'>
 
-        return (
-            <Grid container direction='column' justifyContent='center' alignItems='center'>
-                <Grid item xs={6} className='image2'></Grid>
-                <Grid item xs={6} alignItems='center'>
-                    <Box padding={10}>
-                        <form onSubmit={onSubmit}> 
-                            <Typography variant='h3' gutterBottom  component='h3' align='center' className='text2'>𝙲𝚊𝚍𝚊𝚜𝚝𝚛𝚊𝚛</Typography>
-                            <TextField value={user.name} onChange={(e: ChangeEvent<HTMLInputElement>) =>updatedModel(e)} required id='name' label='Nome' variant='outlined' name='name' margin='normal'fullWidth />
-                            <TextField value={user.username} onChange={(e: ChangeEvent<HTMLInputElement>) =>updatedModel(e)} required id='username' label='Username' variant='outlined' name='username' margin='normal' fullWidth />
-                            <TextField value={user.email} onChange={(e: ChangeEvent<HTMLInputElement>) =>updatedModel(e)} required id='email' label='Email' variant='outlined' name='email' margin='normal' type='email' fullWidth />
-                            <TextField value={user.password} onChange={(e: ChangeEvent<HTMLInputElement>) =>updatedModel(e)} required id='password' label='Senha' variant='outlined' name='password' margin='normal' type='password' fullWidth />
-                            <TextField value={confirmPassword} onChange={(e: ChangeEvent<HTMLInputElement>) =>confirmPasswordHandle(e)}required id='confirmPassword' label='Confirme sua senha' variant='outlined' name='confirmPassword' margin='normal' type='password' fullWidth />
-                            <Box marginTop={2} textAlign='center'>
-                            <Link to='/login' className='text-decorator-none '>
-                                    <Button id='color' variant='contained' className='btnCancelar'>
-                                     Voltar
-                                    </Button>
-                            </Link>    
-                                <Button id='color' type='submit' variant='contained'>
-                                     Cadastrar 
-                                    </Button>
-                            </Box>
-                        </form>
-                    </Box>
-    
-                </Grid>
-    
-    
+                        
+                        <a href='/login' id='text'> <Button id='color2' variant='contained'>
+                                Voltar     
+                                </Button>
+                                </a> 
+                            
+                                
+                            <Button id='color1' type='submit' variant='contained'>
+                                 Cadastrar 
+                                </Button>
+                        </Box>
+                    </form>
+                </Box>
+
             </Grid>
-        );
-    
-    }
 
-export {RegisterUser}
+
+        </Grid>
+    );
+
+}
+
+export {Register}
