2bbc5c985cb2bcdeca2f4f0d64353c6ef8f9e740
DaniNasciment <dannni01@hotmail.com>
2022-12-15 09:07:56 -0300

    feat: centralize itens and change colors
    Co-authored-by: DaniNasciment <dannni01@hotmail.com>
    Co-authored-by: alexsandermog <alexsandermachado15@gmail.com>
    Co-authored-by: Popdollss <roseanemelo17@outlook.com>
    Co-authored-by: KaiqueBourguignon <kaiquegeneration@gmail.com>

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,3 +1,4 @@
+
 import React, {useState, ChangeEvent, useEffect} from 'react';
 import { Grid,Typography, TextField, Button } from '@material-ui/core';
 import {Box} from '@mui/material';
@@ -10,11 +11,14 @@ import { useDispatch } from 'react-redux';
 import { addToken } from '../../store/tokens/actions';
 
 
+
+
 function Login() {
 
     let navigate = useNavigate();
     const dispatch = useDispatch();
     const [token, setToken] = useState('');
+
 
     const [userLogin, setUserLogin] = useState<UserLogin>(
         {
@@ -53,34 +57,34 @@ function Login() {
         }
 
     return (
-        <Grid container direction='row' justifyContent='center' alignItems='center'>
-            <Grid alignItems='center' xs={6}>
+        <Grid container direction='column' justifyContent='center' alignItems='center' className='ajust'>
+            <Grid item xs={6}>
                 <Box paddingX={20}>
                     <form onSubmit={onSubmit}>
-                        <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='textos1'>Entrar</Typography>
+                        <Typography variant='h3' gutterBottom component='h3' align='center' className='ajust1'>𝙶𝚛𝚎𝚎𝚗 𝙷𝚎𝚊𝚛𝚝💚</Typography>
                         <TextField value={userLogin.username} onChange={(e: ChangeEvent<HTMLInputElement>) =>updatedModel(e)} id='username' label='Username' variant='outlined' name='username' margin='normal' fullWidth />
                         <TextField value={userLogin.password} onChange={(e: ChangeEvent<HTMLInputElement>) =>updatedModel(e)} id='password' label='Senha' variant='outlined' name='password' margin='normal' type='password'fullWidth />
                         <Box marginTop={2} textAlign='center'>
                         
-                        <Button type='submit' variant='contained' color='primary'>
-                                    Entrar
+                        <Button id='color' type='submit' variant='contained'  >
+                                Entrar
                                 </Button>
                             
                         </Box>
                     </form>
                     <Box display='flex' justifyContent='center' marginTop={2}>
                         <Box marginRight={1}>
-                            <Typography variant='subtitle1' gutterBottom align='center'>Não tem uma conta?</Typography>
+                            <Typography variant='subtitle1' gutterBottom align='center' className='ajust2' >Não tem uma conta?</Typography>
                         </Box>
                         <Link to='/registeruser'>
-                            <Typography variant='subtitle1' gutterBottom align='center' className='text1'>Cadastre-se</Typography>
+                            <Typography variant='subtitle1' gutterBottom align='center' className='ajust2'>Cadastre-se</Typography>
                             </Link>
                     </Box>
 
                 </Box>
             </Grid>
-            <Grid xs={6} className='image'>
-
+            <Grid item xs={6} className='image'>
+        
             </Grid>
         </Grid>
     );
