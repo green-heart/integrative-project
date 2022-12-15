5c590accef287339d3854d651509f3a7956c7725
Anttonino <victor.antonino12@gmail.com>
2022-12-15 15:21:21 -0300

    feat: create the home page
    Co-authored-by: Anttonino <victor.antonino12@gmail.com>
    Co-authored-by: JoaoVictorCSousa <carvalhojoaovi20@gmail.com>
    Co-authored-by: ArthurMFarias0 <arthuremeunome@gmail.com>

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,15 +1,38 @@
 import React, { useEffect, useState } from 'react';
-import {Typography, Box, Grid, Button} from '@material-ui/core';
+import { Typography, Box, Grid, Button } from '@material-ui/core';
 import './Home.css';
+import { Link } from 'react-router-dom';
+import ListPosting from '../../components/post/listPosting/ListPosting';
+import ListTheme from '../../components/theme/listTheme/ListTheme';
 
+function Home() {
+    return (
+        <Grid container direction='column' justifyContent='center' alignItems='center' className='ajust'>
+            <Grid item xs={6}>
+                <Box display='flex' justifyContent='center' marginTop={2}>
+                    <Box display='flex' justifyContent='center' marginTop={2}>
+                        <Link to="/newTheme">
+                            <Button>
+                                Cadastrar tema
+                            </Button>
+                        </Link>
+                        <Link to="/newPosting">
+                            <Button>
+                                Cadastrar post
+                            </Button>
+                        </Link>
+                    </Box>
+                </Box>
+                <Box>
+                    <ListPosting />
+                    <ListTheme/>
+                </Box>
+            </Grid>
+            <Grid item xs={6} className='image'>
 
-
-function Home(){
-    return(
- 
-                    <img src=""/>
-
+            </Grid>
+        </Grid>
     )
 }
 
-export {Home}
+export { Home }
