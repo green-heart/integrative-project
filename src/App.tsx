cd41e06bb0addb67e691f25173cbbb7b2e0ada5b
Anttonino <victor.antonino12@gmail.com>
2022-12-15 11:41:59 -0300

    feat: add crud routes in app
    Co-authored-by: Anttonino <victor.antonino12@gmail.com>
    Co-authored-by: JoaoVictorCSousa <carvalhojoaovi20@gmail.com>
    Co-authored-by: ArthurMFarias0 <arthuremeunome@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
 import {Navbar} from './components/statics/navbar/Navbar';
 import { RegisterUser } from './pages/register/Register';
 import { Footer } from './components/statics/footer/Footer';
 import { Login } from './pages/login/Login';
 import { Home } from './pages/home/Home';
+import ListPosting from './components/post/listPosting/ListPosting';
+import NewPosting from './components/post/newPosting/NewPosting';
+import DeletePosting from './components/post/deletePosting/DeletePosting'
 
 function App() {
   return (
@@ -18,9 +20,15 @@ function App() {
 
         <Route path="/login" element={<Login  />} />
 
-        <Route path="/home" element={<Home />} />
+        <Route path="/feed" element={<ListPosting />} />
 
         <Route path="/registeruser" element={<RegisterUser />} />
+
+        <Route path="/newPosting" element ={<NewPosting/>} />
+
+        <Route path="/newPosting/:id" element ={<NewPosting/>} />
+
+        <Route path="/deletePosting/:id" element ={<DeletePosting/>} />
                  
       </Routes>
       <Footer />
