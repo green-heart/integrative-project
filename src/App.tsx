079da6dfc6d35b170f3d647d6f510fc0c0aa041e
DaniNasciment <dannni01@hotmail.com>
2022-12-15 08:53:27 -0300

    feat: update routes in aplication
    Co-authored-by: DaniNasciment <dannni01@hotmail.com>
    Co-authored-by: alexsandermog <alexsandermachado15@gmail.com>
    Co-authored-by: Popdollss <roseanemelo17@outlook.com>
    Co-authored-by: KaiqueBourguignon <kaiquegeneration@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,33 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import {Footer} from './components/statics/footer/Footer';
-import {Home} from './pages/home/Home';
 import './App.css';
-import { Title } from './components/title/title';
-import {Login} from './pages/login/Login';
-import { Navbar } from './components/statics/navbar/Navbar';
-import { Posting } from './pages/posting/Posting';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import {Navbar} from './components/statics/navbar/Navbar';
 import { RegisterUser } from './pages/registerUser/RegisterUser';
-import { Provider } from 'react-redux';
-import store from './store/store';
+import { Footer } from './components/statics/footer/Footer';
+import { Login } from './pages/login/Login';
+import { Home } from './pages/home/Home';
+import { Posting } from './pages/posting/Posting';
 
 function App() {
-  const nome = '𝒢𝓇𝑒𝑒𝓃 𝐻𝑒𝒶𝓇𝓉 💚'
-
   return (
-    <Provider store={store}>
     <Router>
       <Navbar />
-      
       <Routes>
         
+        <Route path="/" element={<Login  />} />
+
         <Route path="/login" element={<Login  />} />
 
         <Route path="/home" element={<Home />} />
 
-        <Route path="/posting" element={<Posting />} />
-
         <Route path="/registeruser" element={<RegisterUser />} />
-        
+
+        <Route path="/posting" element={<Posting />} />
+                 
       </Routes>
       <Footer />
     </Router>
-    </Provider>
   );
 }
 
