2cd3427da57a406070c3d2e13d2089fabdaa389a
Arthur <arthuremeunome@gmail.com>
2022-12-15 13:28:34 -0300

    feat: create new theme component Co-authored-by: ArthurMFarias0 <arthuremeunome@gmail.com> Co-authored-by: JoaoVictorCSousa <carvalhojoaovi20@gmail.com> Co-authored-by: Anttonino <victor.antonino12@gmail.com>

---
+++ src/components/theme/newTheme/NewTheme.tsx
@@ -0,0 +1,119 @@
+import { Button, Container, TextField, Typography } from "@material-ui/core";
+import { ChangeEvent, useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
+import { toast } from "react-toastify";
+import Theme from "../../../models/Theme";
+import { post, put, searchById } from "../../../services/Service";
+import { TokenState } from "../../../store/tokens/tokensReducer";
+
+function NewTheme(){
+    let navigate = useNavigate()
+    const { id } = useParams<{id: string}>();
+    const token = useSelector<TokenState, TokenState["tokens"]>(
+        (state) => state.tokens
+    );
+    const [theme, setTheme] = useState<Theme>({
+        id: 0,
+        classification: '',
+        types: ''
+    })
+
+    useEffect(() => {
+        if (token == "") {
+            toast.error ('Você precisa estar logado', {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: false,
+                draggable: false,
+                theme: "colored",
+                progress: undefined,
+            });
+            navigate ("/login")
+        }
+    }, [token])
+
+    useEffect(() => {
+        if(id !== undefined){
+            findById(id)
+        }
+    }, [id])
+
+    async function findById(id:string) {
+        searchById(`/theme/${id}`, setTheme, {
+            headers: {
+                'Authorization': token
+            }
+        })
+    }
+
+    function updatedTheme(e: ChangeEvent<HTMLInputElement>){
+
+        setTheme({
+            ...theme,
+            [e.target.name]: e.target.value,
+        })
+    }
+
+    async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
+        e.preventDefault()
+        console.log("theme" + JSON.stringify(theme))
+
+        if (id !== undefined) {
+            console.log(theme)
+            put(`/theme`, theme, setTheme, {
+                headers: {
+                    'Authorization': token
+                }
+            })
+            toast.success('Tema atualizado com sucesso', {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: false,
+                draggable: false,
+                theme: "colored",
+                progress: undefined,
+            });
+
+        } else {
+            post(`/theme`, theme, setTheme, {
+                headers: {
+                    'Authorization': token
+                }
+            })
+            toast.success('Tema cadastrado com sucesso', {
+                position: "top-right",
+                    autoClose: 2000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: false,
+                    draggable: false,
+                    theme: "colored",
+                    progress: undefined,
+            });
+        }
+        back()
+    }
+
+    function back() {
+        navigate('/themes')
+    }
+    
+    return (
+        <Container maxWidth="sm" className="top">
+        <form onSubmit={onSubmit}>
+            <Typography variant="h3" color="textSecondary" component="h1" align="center" >Formulário de cadastro tema</Typography>
+            <TextField value={theme.classification} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedTheme(e)} id="classification" label="classification" variant="outlined" name="classification" margin="normal" fullWidth />
+            <Button type="submit" variant="contained" color="primary">
+                Finalizar
+            </Button>
+        </form>
+    </Container>
+    )
+}
+
+export default NewTheme;
