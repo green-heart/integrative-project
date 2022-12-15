ff9133b4b52553f07b2cfb8589b1bd594c7f0648
Anttonino <victor.antonino12@gmail.com>
2022-12-15 16:42:52 -0300

    feat: change the footer style
    Co-authored-by: Anttonino <victor.antonino12@gmail.com>
    Co-authored-by: alexsandermog <alexsandermachado15@gmail.com>

--- src/components/statics/footer/Footer.tsx
+++ src/components/statics/footer/Footer.tsx
@@ -8,18 +8,9 @@ import './Footer.css';
 function Footer(){ 
     return(
         <>
-        <Grid container direction="row" justifyContent="center" alignItems="center">
-                <Grid alignItems="center" item xs={12}>
-                    <Box className='box1'>
-                        <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center" >
-                            <Typography variant="h5" align="center" gutterBottom className='textos'></Typography>
-                        </Box>
-                    </Box>
-                    <Box className='box1'>
-                        <Box paddingTop={1}>
-                            <Typography variant="subtitle2" align="center" gutterBottom className='textos' >©2022 Copyright</Typography>
-                        </Box>
-                    </Box>
+        <Grid container justifyContent="center" alignItems="center">
+                <Grid alignItems="center" justifyContent='center' item xs={12} style={{backgroundColor: '#1c3f2b', height: '60px'}}>
+                        <Typography variant="subtitle2" align="center" className='textos' style={{marginTop: 20}}>Green Heart © Copyright 2022</Typography>
                 </Grid>
             </Grid>
         </>
