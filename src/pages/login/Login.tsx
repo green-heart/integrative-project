ac21a8b169639d03cbda842560b885ae19484a8c
Arthur <arthuremeunome@gmail.com>
2022-12-16 11:10:48 -0300

    fix: remove console errors in Login page

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -3,6 +3,7 @@ import { Grid,Typography, TextField, Button } from '@material-ui/core';
 import {Box} from '@mui/material';
 import { Link, useNavigate } from 'react-router-dom';
 import {api, login} from '../../services/Service';
+import useLocalStorage from 'react-use-localstorage';
 import './Login.css';
 import SignIn from '../../models/SignIn';
 import { useDispatch } from 'react-redux';
@@ -14,7 +15,6 @@ function Login() {
     let navigate = useNavigate();
     const dispatch = useDispatch();
     const [token, setToken] = useState('');
-
 
     const [userLogin, setUserLogin] = useState<SignIn>(
         {
@@ -58,14 +58,14 @@ function Login() {
                 <Box paddingX={20}>
                     <form onSubmit={onSubmit}>
                         <Typography variant='h3' gutterBottom component='h3' align='center' className='ajust1'>𝙶𝚛𝚎𝚎𝚗 𝙷𝚎𝚊𝚛𝚝💚</Typography>
-                        <TextField value={userLogin.username} onChange={(e: ChangeEvent<HTMLInputElement>) =>updatedModel(e)} id='username' label='Username' variant='outlined' name='username' margin='normal' fullWidth />
-                        <TextField value={userLogin.password} onChange={(e: ChangeEvent<HTMLInputElement>) =>updatedModel(e)} id='password' label='Senha' variant='outlined' name='password' margin='normal' type='password'fullWidth />
+                        <TextField value={userLogin.username} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='username' label='Username' variant='outlined' name='username' margin='normal' fullWidth />
+                        <TextField value={userLogin.password} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='password' label='Senha' variant='outlined' name='password' margin='normal' type='password' fullWidth />
                         <Box marginTop={2} textAlign='center'>
-                        
-                        <Button id='color3' type='submit' variant='contained'  >
+
+                            <Button id='color3' type='submit' variant='contained'  >
                                 Entrar
-                                </Button>
-                            
+                            </Button>
+
                         </Box>
                     </form>
                     <Box display='flex' justifyContent='center' marginTop={2}>
@@ -74,16 +74,16 @@ function Login() {
                         </Box>
                         <Link to='/registeruser'>
                             <Typography variant='subtitle1' gutterBottom align='center' className='ajust2'>Cadastre-se</Typography>
-                            </Link>
+                        </Link>
                     </Box>
 
                 </Box>
             </Grid>
             <Grid item xs={6} className='image'>
-        
+
             </Grid>
         </Grid>
     );
 }
 
-export {Login}
+export { Login }
