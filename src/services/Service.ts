b8da9778ec356f73fd671582f830152d7c0d883d
Arthur <arthuremeunome@gmail.com>
2022-12-16 11:12:00 -0300

    fix: Service errors

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import Login from '../models/SignIn';
+import { Dispatch, SetStateAction } from 'react';
 
 export const api = axios.create({
     baseURL: 'http://localhost:4000'
@@ -20,14 +21,14 @@ export const login = async (url: string, data: {
     username: string,
     password: string
 },
-    setData: any) => {
+    setData: React.Dispatch<React.SetStateAction<string>>) => {
     const response = await api.post(url, data)
     setData(response.data.token)
 }
 
 export const search = async (url: string, setData: any, header: any) => {
     const response = await api.get(url, header)
-    setData (response.data)
+    setData(response.data)
 }
 
 export const searchById = async (url: string, setData: any, header: any) => {
