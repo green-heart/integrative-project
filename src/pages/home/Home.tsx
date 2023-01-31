b53e44d3e084e826baebaabbf97e195efb95b8e8
Anttonino <victor.antonino12@gmail.com>
2023-01-31 13:13:27 -0300

    feat: create the initial modal struct

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,21 +1,52 @@
 import React, { useEffect, useState } from 'react';
-import { Box, Grid, Button } from '@material-ui/core';
 import './Home.css';
 import { Link } from 'react-router-dom';
 import ListPosting from '../../components/post/listPosting/ListPosting';
 import ListTheme from '../../components/theme/listTheme/ListTheme';
+import Modal from '@mui/material/Modal';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import NewPosting from '../../components/post/newPosting/NewPosting';
 
 function Home() {
+
+    const style = {
+        position: 'absolute' as 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        bgcolor: 'background.paper',
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
+    };
+
+        const [open, setOpen] = React.useState(false);
+        const handleOpen = () => setOpen(true);
+        const handleClose = () => setOpen(false);
+
     return (
         <Grid container direction='column' justifyContent='center' alignItems='center' className='ajust'>
             <Grid item xs={12} className="w-75">
                 <Box display='flex' justifyContent='center' marginTop={2}>
                     <Box display='flex' justifyContent='center' marginTop={2}>
-                        <Link to="/newPosting" id='text1' className='w-50'>
-                            <Button className="w-50" style={{ marginLeft: 250, marginTop: 10 }} >
-                                Começar nova postagem
-                            </Button>
-                        </Link>
+                            <Button onClick={handleOpen}>Open modal</Button>
+                            <Modal
+                                open={open}
+                                onClose={handleClose}
+                                aria-labelledby="modal-modal-title"
+                                aria-describedby="modal-modal-description"
+                            >
+                                <Box sx={style}>
+                                    <Typography id="modal-modal-title" variant="h6" component="h2">
+                                        Text in a modal
+                                    </Typography>
+                                    <NewPosting/>
+                                </Box>
+                            </Modal>
                     </Box>
                 </Box>
                 <Box>
